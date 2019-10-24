367427d86c21b22b54a2f94c982fc1d3667675b3
Jessy Anglehart-Nunes <ges13596@gmail.com>
2019-10-24 18:09:49 -0400

    remove google cloud artifacts and simplify graphql schema

--- api/src/resolvers/Query.js
+++ api/src/resolvers/Query.js
@@ -1,71 +1,26 @@
-const {
-  Connector,
-  datasets,
-  tables
-} = require('../dal');
-
+// eslint-disable-next-line no-unused-vars
 const blocks = async (root, args, context, info) => {
-  const connector = new Connector(datasets.blockchain, tables.blocks, args);
-  const results = await connector.run();
-  return results;
-}
+  throw new Error('not implemented');
+};
 
+// eslint-disable-next-line no-unused-vars
 const inputs = async (root, args, context, info) => {
-  const connector = new Connector(datasets.blockchain, tables.inputs);
-  const results = await connector.run();
-  return results;
-}
+  throw new Error('not implemented');
+};
 
+// eslint-disable-next-line no-unused-vars
 const outputs = async (root, args, context, info) => {
-  const connector = new Connector(datasets.blockchain, tables.outputs);
-  const results = await connector.run();
-  return results;
-}
+  throw new Error('not implemented');
+};
 
+// eslint-disable-next-line no-unused-vars
 const transactions = async (root, args, context, info) => {
-  const connector = new Connector(datasets.blockchain, tables.transactions, args);
-  const results = await connector.run();
-  return results;
-}
-
-const commits = async (root, args, context, info) => {
-  const connector = new Connector(datasets.github, tables.commits);
-  const results = await connector.run();
-  return results;
-}
-
-const contents = async (root, args, context, info) => {
-  const connector = new Connector(datasets.github, tables.contents);
-  const results = await connector.run();
-  return results;
-}
-
-const files = async (root, args, context, info) => {
-  const connector = new Connector(datasets.github, tables.files);
-  const results = await connector.run();
-  return results;
-}
-
-const languages = async (root, args, context, info) => {
-  const connector = new Connector(datasets.github, tables.languages);
-  const results = await connector.run();
-  return results;
-}
-
-const licenses = async (root, args, context, info) => {
-  const connector = new Connector(datasets.github, tables.licenses);
-  const results = await connector.run();
-  return results;
-}
+  throw new Error('not implemented');
+};
 
 module.exports = {
   blocks,
   inputs,
   outputs,
-  transactions,
-  commits,
-  contents,
-  files,
-  languages,
-  licenses
-}
+  transactions
+};
